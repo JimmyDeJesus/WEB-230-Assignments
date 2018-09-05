52762f754fb6a9008ac72d06ce85324f0c9efe5f
JimmyDeJesus <32549140+JimmyDeJesus@users.noreply.github.com>
2018-09-05 19:01:42 -0400

    added concatenation folder

---
+++ series/concatenation/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$day = 'Thursday';
+$date = 31;
+$year = 2011;
+
+echo 'The date is <strong>'.$day.' '.$date.' '.$year.'<strong>';
+
+echo '<br/><br/>';
+
+echo "The date is <strong> $day $date $year<strong>";
+
+print '<br/><br/>';
+
+print 'revisting print';
+
+
+ ?>

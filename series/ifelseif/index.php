dc60a20e450fc65d8c05ae70e14dcfe5cc1b4f58
JimmyDeJesus <32549140+JimmyDeJesus@users.noreply.github.com>
2018-09-05 20:19:36 -0400

    adding ifelseif folder to master

---
+++ series/ifelseif/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$number = 18;
+
+if($number==10) {
+  echo 'Equal to ten.';
+} else if ($number==18) {
+
+  echo 'Equal to eightteen.';
+
+} else {
+
+  echo 'Not equal.';
+
+  }
+
+
+
+
+
+?>

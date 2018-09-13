b8f089a61da0f9d6030dd0c5ff92b4f8a5485930
JimmyDeJesus <32549140+JimmyDeJesus@users.noreply.github.com>
2018-09-12 22:29:51 -0400

    Adding the arithmetic folder file to the master

---
+++ series/arithmetic/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$sum = 10 + 20;
+  echo $sum;
+/*End of first number 30*/
+$number1 = 100;
+$number2 = 50;
+$number3 = 2;
+  echo '<br></br>';
+    $result = $number1 + $number2 / $number3;
+      echo $result;
+/*end of second number 125*/
+$number4 = 10;
+$number5 = 3;
+/*finding out the remainder of $number4 and $number5 aka 10/3= remainder 1. */
+  $result = $number4%$number5;
+    echo '<br></br>';
+    echo $result;
+
+
+$number7 = 10;
+
+$number7++;
+  echo '<br></br>';
+  echo $number7;
+
+?>

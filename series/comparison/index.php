b871eb2b26f8f8b83bb260659dd6e9ee8fb8fef7
JimmyDeJesus <32549140+JimmyDeJesus@users.noreply.github.com>
2018-09-12 20:13:10 -0400

    Adding the comparison files to the master

---
+++ series/comparison/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$number1 = 10;
+$number2 = 9;
+
+  if ($number1>=$number2) {
+    echo 'Yes.';
+}else {
+    echo 'No.';
+}
+
+$password = 'password';
+  if ($password==$password) {
+    echo 'Correct!';
+}else {
+    echo 'Incorrect!';
+}
+
+?>
